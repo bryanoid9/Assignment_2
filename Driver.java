64a7c93ad7630d7f478de0957e7f5a1db758f8b7
bryanoid9 <90728547+bryanoid9@users.noreply.github.com>
2023-06-25 23:25:00 -0700

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,11 @@
+package Twitter;
+
+
+public class Driver {
+    public static void main(String[] args) {
+    	// Create an instance of the AdminControlPanel
+        AdminControlPanel adminPanel = AdminControlPanel.getInstance();
+     // Initialize the GUI of the admin panel
+        adminPanel.initializeGUI();
+    }
+}
